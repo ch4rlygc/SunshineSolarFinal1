68df549b09e5c17e467f0201e80e913f45103c0a
carlos <carlos.jgc14@gmail.com>
2018-09-20 19:41:08 -0600

    Commit inicial

---
+++ js/products.js
@@ -0,0 +1,49 @@
+var panels = ['280W Solar Panel',
+              '260W Solar Panel',
+              '100W Solar Panel',
+              '80W Solar Panel',
+              '60W Solar Panel',
+              'Silicon',
+              'German Technology'];
+
+var buttonMoreDetails = {
+    spanish : "Ver más detalles",
+    english : "See more details"
+}
+
+var buttonClose = {
+    spanish : "Cerrar",
+    english : "Close"
+}
+
+var buttonCheckPrice = {
+    spanish : "Cotizar",
+    english : "Check the Price"
+}
+
+var panel280 = {
+  name: "280W Solar Panel",
+  watts : "280W",
+  voltage : "35.OV",
+  nombre : "Panel solar de 280W"
+}
+
+var panel260 = {
+    name: "260W Solar Panel",
+    watts : "260W",
+    voltage : "29.5",
+    nombre : "Panel solar de 260W"
+}
+
+var solarPanelsText = {
+    spanish : "this is a test text",
+    english : "Solar panels with German technology of 10, 15, 15, 20, 40, 60 watts and more..."
+}
+
+function drawPanels () {
+
+}
+
+
+
+
---
+++ quote.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" href="img/fav-icon.png" type="image/x-icon" />
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>sushinesolar</title>
+
+    <!-- Icon css link -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Rev slider css -->
+    <link href="vendors/revolution/css/settings.css" rel="stylesheet">
+    <link href="vendors/revolution/css/layers.css" rel="stylesheet">
+    <link href="vendors/revolution/css/navigation.css" rel="stylesheet">
+
+    <!-- Extra plugin css -->
+    <link href="vendors/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+    <link href="vendors/magnify-popup/magnific-popup.css" rel="stylesheet">
+
+
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+</head>
+
+<body>
+   <center class="father">
+       
+        <section class="get_in_touch_area p_100">
+            <div class="container ">
+                <div class="get_touch_inner">
+                    <div class="col-lg-6">
+                        <form class="contact_us_form row" action="" method="post" id="contactForm" novalidate="novalidate">
+                           <div class="form-group col-sm-3">
+                               <input type="text" class="form-control" id="id_product" name="id_product" placeholder="Id Product">
+                           </div>
+                           <div class="form-group col-sm-3">
+                               <input type="number" class="form-control" id="quantity" name="quantity" placeholder="Quantity" min="1" value="1" required>
+                           </div>    
+                           
+                            <div class="form-group col-lg-12">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name"> 
+                           </div>
+                            <div class="form-group col-lg-6">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                            </div>
+                            <div class="form-group col-lg-6">
+                                <input type="tel" class="form-control" id="number" name="number" placeholder="Telephone">
+                            </div>
+
+                            <div class="form-group col-lg-6">
+                                <input type="text" class="form-control" id="company" name="company" placeholder="Company">
+                            </div>
+                            <div class="form-group col-lg-6">
+                               <input type="text" class="form-control" id="city" name="city" placeholder="Country Department or City" required>
+                           </div>
+
+                            <div class="form-group col-lg-12">
+                                <button type="submit" value="submit" class="btn submit_btn form-control" >Quote</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+   </center>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Rev slider js -->
+    <script src="vendors/revolution/js/jquery.themepunch.tools.min.js"></script>
+    <script src="vendors/revolution/js/jquery.themepunch.revolution.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="vendors/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <!-- Extra plugin css -->
+    <script src="vendors/counterup/jquery.waypoints.min.js"></script>
+    <script src="vendors/counterup/jquery.counterup.min.js"></script>
+    <script src="vendors/counterup/apear.js"></script>
+    <script src="vendors/counterup/countto.js"></script>
+    <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+    <script src="vendors/magnify-popup/jquery.magnific-popup.min.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="vendors/circle-bar/circle-progress.min.js"></script>
+    <script src="vendors/circle-bar/plugins.js"></script>
+    <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+    <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+
+    <!--gmaps Js-->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+    <script src="js/gmaps.min.js"></script>
+
+    <!-- contact js -->
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/contact.js"></script>
+
+    <script src="js/circle-active.js"></script>
+    <script src="js/theme.js"></script>
+</body>
+
+</html>
